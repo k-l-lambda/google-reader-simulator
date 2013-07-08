fc1f9237c63e97312f3ec083feefe96d7b7f4c34
xxxk.l.xxx@gmail.com <k.l.lambda@gmail.com>
2013-07-08 04:40:25 +0000

    ReaderGAE\Source\templates\View.html: user id from google user server added.

--- ReaderGAE/Source/main.py
+++ ReaderGAE/Source/main.py
@@ -4,12 +4,18 @@ import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
 
 
 class TestHandler(webapp.RequestHandler):
     def get(self):
+        user = users.get_current_user()
+
         path = os.path.join(os.path.dirname(__file__), 'templates/View.html')
-        self.response.out.write(template.render(path, {}))
+        self.response.out.write(template.render(path, {
+            'user':                 user,
+            'logout_url':           users.create_logout_url(self.request.uri),
+        }))
 
 
 def main():
